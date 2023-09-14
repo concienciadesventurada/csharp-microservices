6e259a265e110b18de173963c582a8b5c18d2461
concienciadesventurada <porquemarx@gmail.com>
2023-09-14 14:53:38 -0600

    added delete method and coupondelete method

--- Web/Controllers/CouponController.cs
+++ Web/Controllers/CouponController.cs
@@ -24,8 +24,41 @@ namespace Web.Controllers {
             return View(coupons);
         }
 
-        public async Task<IActionResult> CouponCreate() {
-            return View();
+        public async Task<IActionResult> CouponCreate() => View();
+
+        // NOTE: The same way the brackets tell the method in the API controller
+        // it'll work the same, with the exception of it taking the model
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDTO model) {
+            if (ModelState.IsValid) {
+                ResponseDTO? res = await _couponService.CreateCouponAsync(model);
+
+                if (res != null && res.IsSuccess) {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> CouponDelete(int id) {
+            ResponseDTO? res = await _couponService.GetCouponByIdAsync(id);
+
+            if (res != null && res.IsSuccess) {
+                CouponDTO? model = JsonConvert.DeserializeObject<CouponDTO>(Convert.ToString(res.Result)!);
+                return View(model);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CouponDelete(CouponDTO couponDto) {
+            ResponseDTO? res = await _couponService.DeleteCouponAsync(couponDto.CouponId);
+
+            if (res != null && res.IsSuccess) return RedirectToAction(nameof(CouponIndex));
+
+            return View(couponDto);
         }
     }
 }
