3c6cd6c1a293d19b636f03e7fdaf126f807a4d17
concienciadesventurada <porquemarx@gmail.com>
2023-09-15 21:05:35 -0600

    added user manager, db context and role manager dependency injections in auth service

--- Services/AuthAPI/Service/AuthService.cs
+++ Services/AuthAPI/Service/AuthService.cs
@@ -1,8 +1,27 @@
+using Microsoft.AspNetCore.Identity;
+
+using Services.AuthAPI.Data;
+using Services.AuthAPI.Models;
 using Services.AuthAPI.Models.DTO;
 using Services.AuthAPI.Service.IService;
 
 namespace Services.AuthAPI.Service {
     public class AuthService : IAuthUser {
+        private readonly AppDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        // NOTE: All these are dependency injection that Identity already
+        // provides for managing users
+        public AuthService(AppDbContext db,
+                UserManager<ApplicationUser> userManager,
+                RoleManager<IdentityRole> roleManager
+            ) {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
         public Task<UserDTO> Register(RegistrationRequestDTO registerReqDto) {
             throw new NotImplementedException();
         }
